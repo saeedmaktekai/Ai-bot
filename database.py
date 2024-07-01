0585e8e85b4170f9f4de36c2ef428e4cb2247480
SAEED <get.maktek@gmail.com>
2024-07-01 23:22:07 +0500

    fastapi starting

--- database.py
+++ database.py
@@ -11,6 +11,8 @@ from langchain_openai import OpenAIEmbeddings
 import os
 import chromadb
 openai_api_key=os.getenv("OPENAI_API_KEY")
+from fastapi import FastAPI
+app = FastAPI
 
 embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key,model="text-embedding-3-large")
 def injectdata(path):
@@ -23,25 +25,30 @@ def injectdata(path):
    data = openai_lc_client = Chroma.from_documents(
    docs, embeddings, client=new_client, collection_name="openai_collection"
    )
-   Chroma.from_documents(docs, embeddings, persist_directory="./chroma_db_openai")
+   Chroma.from_documents(docs, embeddings, persist_directory="./db")
    return docs 
+
+@app.post("/upload/")
+
+
+
+
 
 def retreiveembedding(query):
    db3 = Chroma(persist_directory="./chroma_db_openai", embedding_function=embeddings)
    docs = db3.similarity_search(query)
    return docs
-
-   
-
-
-
-
-
 def lookforsimilaritysearch(query):
    # load from disk
    data = Chroma(persist_directory="./chroma_db_openai", embedding_function=embeddings)
    data.similarity_search(query)
    return data
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
 
 
--- loadfromchroma.py
+++ loadfromchroma.py
@@ -1,11 +1,11 @@
 from database import injectdata
 from database import retreiveembedding
-# path="/Users/saeedanwar/Desktop/Ai-bot/data"
-# injectdata(path)
+path="/Users/saeedanwar/Desktop/Ai-bot/data"
+injectdata(path)
 
-query="who is marjan "
-smsearch=retreiveembedding(query)
-print(smsearch)
+# query="who is marjan "
+# smsearch=retreiveembedding(query)
+# print(smsearch)
 # print(type(smsearch))
 
 
